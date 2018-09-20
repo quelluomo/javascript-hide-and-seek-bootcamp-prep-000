d73d2c121dc315b3b4e15effab30a429efb2f3fe
Matteo Muscas <thatman.j@gmail.com>
2018-09-20 14:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+}
+function increaseRankBy(n) {
+  var rank = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+      for (let i = 0; i < rank.length; i++) {
+      rank[i].innerHTML = parseInt(rank[i].innerHTML)+n;
+    }
+}
+function deepestChild(){
+  var rank = document.getElementById('grand-node').querySelectorAll('div');
+    var inception;
+    for (let i = 0; i < rank.length; i++) {
+      inception = rank[i].querySelector('div');
+    }
+    return rank;
+}
+function deepestChild() {
+  let parent = document.querySelector('div#grand-node');
+  let child = parent.querySelectorAll('div')[3];
+  return parent;
+}

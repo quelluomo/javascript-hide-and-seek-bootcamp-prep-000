65970048c5594e8a29c1deae0b996f01cf815a51
Matteo Muscas <thatman.j@gmail.com>
2018-09-20 14:31:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+}
+function increaseRankBy(n) {
+  var rank = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+      for (let i = 0; i < rank.length; i++) {
+      rank[i].innerHTML = parseInt(rank[i].innerHTML)+n;
+    }
+}
+function deepestChild() {
+  var inception = document.getElementById('grand-node');
+  var nolan = inception.children[0];
+  while (nolan) {
+    inception = nolan;
+    nolan = inception.children[0];
+  }
+  return inception;
+} 
